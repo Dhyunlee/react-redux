1265e4399c862c1e744d7a547dbb4dea9159d76b
Donghyun Lee <skyblue5030@naver.com>
2022-05-31 07:59:43 +0900

    8. 할일 목록 구현(1) - Presentational 컴포넌트 만들기

---
+++ learn-redux/src/components/Todos.jsx
@@ -0,0 +1,50 @@
+import React, { useState } from 'react';
+
+function TodoItem({ todo, onToggle }) {
+  return (
+    <li
+      style={{
+        textDecoration: todo.done ? 'line-through' : 'none',
+      }}
+      onClick={() => onToggle(todo.id)}
+    >
+      {todo.text}
+    </li>
+  );
+}
+
+function TodoList({ todos, onToggle }) {
+  return (
+    <ul>
+      {todos.map(todo => (
+        <TodoItem key={todo.id} todo={todo} onToggle={onToggle} />
+      ))}
+    </ul>
+  );
+}
+
+function Todos({ todos, onCreate, onToggle }) {
+  const [text, setText] = useState('');
+
+  const onChagne = e => setText(e.target.value);
+  const onSubmit = e => {
+    e.preventDefault();
+    onCreate(text);
+    setText('');
+  };
+  return (
+    <div>
+      <form onSubmit={onSubmit}>
+        <input
+          value={text}
+          onChange={onChagne}
+          placeholder="할 일을 입력해주세요 😀"
+        />
+        <button type="submit">등록</button>
+      </form>
+      <TodoList todos={todos} onToggle={onToggle} />
+    </div>
+  );
+}
+
+export default Todos;
