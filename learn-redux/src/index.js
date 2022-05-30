44f7838b0255c254743192d836a63c4abcc7c01f
Donghyun Lee <skyblue5030@naver.com>
2022-05-31 07:29:43 +0900

    7. 리덕스 개발자도구 사용하기

--- learn-redux/src/index.js
+++ learn-redux/src/index.js
@@ -5,9 +5,9 @@ import App from './App';
 import { legacy_createStore as createStore } from 'redux';
 import { Provider } from 'react-redux';
 import rootReducer from './modules';
-
+import { composeWithDevTools } from 'redux-devtools-extension';
 // 스토어 생성
-const store = createStore(rootReducer);
+const store = createStore(rootReducer, composeWithDevTools());
 // console.log(store.getState()); //스토어의 상태를 확인
 
 ReactDOM.render(
