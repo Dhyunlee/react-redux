011ec66c8456b6362961a00f817455589b0e64f7
Donghyun Lee <skyblue5030@naver.com>
2022-05-30 17:08:26 +0900

    5. 리덕스 모듈 만들기

--- learn-redux/src/index.js
+++ learn-redux/src/index.js
@@ -2,11 +2,19 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import './exercise';
+import { legacy_createStore as createStore } from 'redux';
+import { Provider } from 'react-redux';
+import rootReducer from './modules';
+
+// 스토어 생성
+const store = createStore(rootReducer);
+// console.log(store.getState()); //스토어의 상태를 확인
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root'),
 );
