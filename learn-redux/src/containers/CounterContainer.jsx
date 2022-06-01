6c188095cd736ae74b302aad8851bb2e5af98c01
Donghyun Lee <skyblue5030@naver.com>
2022-06-01 17:24:42 +0900

    11. useSelector 최적화하기

--- learn-redux/src/containers/CounterContainer.jsx
+++ learn-redux/src/containers/CounterContainer.jsx
@@ -1,15 +1,28 @@
 import React from 'react';
-import { useSelector, useDispatch } from 'react-redux';
+import { useSelector, useDispatch, shallowEqual } from 'react-redux';
 import Counter from '../components/Counter';
 import { increase, decrease, setDiff } from '../modules/counter';
 
 function CounterContainer() {
-  const { number, diff } = useSelector(state => ({
-    // 여기 파라미터로 전달된 state는 리덕스의 현재 state이다.
-    number: state.counter.number,
-    diff: state.counter.diff,
-  }));
+  const { number, diff } = useSelector(
+    state => ({
+      number: state.counter.number,
+      diff: state.counter.diff,
+    }),
+    /*     
+      (left, right) => {
+        return left.diff === right.diff && left.number === right.number;
+      },
+      ※ 매번 이렇게 작성해서 비교해주는 것보다 shallowEqual 함수를
+        사용하는 것이다. 
+        사용법은 shallowEqual를 불러와서 두번째 파라미터에 넣어주면 된다.
+    */
+    shallowEqual,
+  );
 
+  // Container 최적화하는 방법
+  // const number = useSelector(state => state.counter.number);
+  // const diff = useSelector(state => state.counter.diff);
   const dispatch = useDispatch();
 
   // 디스패치하는 함수들 생성
